15265387c60b54ed96a3e95d2c600732066e240f
wandl <hnxxyhcwdl1003@163.com>
2021-02-10 00:36:05 +0800

    优化异常处理

--- src/main/java/org/apache/shiro/spring/boot/qrcode/authc/QrcodeAuthenticatingFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/qrcode/authc/QrcodeAuthenticatingFilter.java
@@ -82,12 +82,12 @@ public class QrcodeAuthenticatingFilter extends AbstractTrustableAuthenticatingF
 					LOG.trace(mString);
 				}
 				
-				WebUtils.toHttp(response).setStatus(HttpStatus.SC_BAD_REQUEST);
+				WebUtils.toHttp(response).setStatus(HttpStatus.SC_OK);
 				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 				response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 				
 				// Response Authentication status information
-				JSONObject.writeJSONString(response.getWriter(), AuthcResponse.fail(mString));
+				JSONObject.writeJSONString(response.getWriter(), AuthcResponse.fail(HttpStatus.SC_BAD_REQUEST, mString));
 				
 				return false;
 			}
@@ -103,12 +103,12 @@ public class QrcodeAuthenticatingFilter extends AbstractTrustableAuthenticatingF
 			// Ajax 请求：响应json数据对象
 			if (WebUtils.isAjaxRequest(request)) {
 				
-				WebUtils.toHttp(response).setStatus(HttpStatus.SC_UNAUTHORIZED);
+				WebUtils.toHttp(response).setStatus(HttpStatus.SC_OK);
 				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 				response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 				
 				// Response Authentication status information
-				JSONObject.writeJSONString(response.getWriter(), AuthcResponse.fail(mString));
+				JSONObject.writeJSONString(response.getWriter(), AuthcResponse.fail(HttpStatus.SC_UNAUTHORIZED, mString));
 				
 				return false;
 			}
