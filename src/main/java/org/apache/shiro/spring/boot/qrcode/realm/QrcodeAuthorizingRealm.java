44a8e1ba8f02138f970b7ca7c33f72fcd0c7e03f
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:06:21 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.2.x branch

--- src/main/java/org/apache/shiro/spring/boot/qrcode/realm/QrcodeAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/qrcode/realm/QrcodeAuthorizingRealm.java
@@ -5,7 +5,7 @@ import org.apache.shiro.spring.boot.qrcode.token.QrcodeAuthenticationToken;
 
 /**
  * Qrcode AuthorizingRealm
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class QrcodeAuthorizingRealm extends AbstractAuthorizingRealm {
 
--- src/test/java/org/apache/shiro/spring/boot/qrcode/QrcodePrincipalTest.java
+++ src/test/java/org/apache/shiro/spring/boot/qrcode/QrcodePrincipalTest.java
@@ -13,17 +13,26 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.apache.shiro.spring.boot.qrcode.authc;
+package org.apache.shiro.spring.boot.qrcode;
 
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.spring.boot.qrcode.QrcodeInfo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * TODO
+ * Unit tests for {{ @link QrcodePrincipal }}.
+ *
  * @author [@Loong Wan](https://github.com/loong10k)
+ * @since 1.0.0
  */
-public interface QrcodeRecognitionProvider {
+@DisplayName("QrcodePrincipal Tests")
+class QrcodePrincipalTest {
 
-	QrcodeInfo loadQrcodeInfo(AuthenticationToken token);
-	
+    @Test
+    @DisplayName("Instance can be created via constructor")
+    void testInstantiation() {
+        QrcodePrincipal instance = new QrcodePrincipal();
+        assertThat(instance).isNotNull();
+    }
 }
