d3c57a1360c626308883c0a318598854bb6dbcad
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:44 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/main/java/org/apache/shiro/spring/boot/qrcode/authc/QrcodeRecognitionProvider.java
+++ src/main/java/org/apache/shiro/spring/boot/qrcode/authc/QrcodeRecognitionProvider.java
@@ -20,7 +20,7 @@ import org.apache.shiro.spring.boot.qrcode.QrcodeInfo;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public interface QrcodeRecognitionProvider {
 
--- src/test/java/org/apache/shiro/spring/boot/qrcode/authc/QrcodeAuthenticatingFilterTest.java
+++ src/test/java/org/apache/shiro/spring/boot/qrcode/authc/QrcodeAuthenticatingFilterTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link QrcodeAuthenticatingFilter }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("QrcodeAuthenticatingFilter Tests")
