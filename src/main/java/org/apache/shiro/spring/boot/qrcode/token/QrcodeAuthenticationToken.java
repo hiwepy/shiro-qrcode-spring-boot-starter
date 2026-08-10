4dbd9e0b9a52fb9f9a15f5f43c3f8f4b0a6c29c1
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:15:27 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/qrcode/token/QrcodeAuthenticationToken.java
+++ src/main/java/org/apache/shiro/spring/boot/qrcode/token/QrcodeAuthenticationToken.java
@@ -19,7 +19,7 @@ import org.apache.shiro.biz.authc.token.DefaultAuthenticationToken;
 
 /**
  * Qrcode Authentication Token
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @SuppressWarnings("serial")
 public class QrcodeAuthenticationToken extends DefaultAuthenticationToken {
